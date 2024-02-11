69ee5626a8940614ea0d517f6aa27dfebc134955
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-02-11 20:51:03 +0600

    Add files via upload

---
+++ Course54 (javascript and node.js)/new.js
@@ -0,0 +1,4 @@
+console.log("Hello world")
+console.log("Hey Im cosole")
+console.log("this is second")
+console.log("Hey Im cosole")
---
+++ Course54 (javascript and node.js)/script.js
@@ -0,0 +1,21 @@
+alert("page refreshed");
+
+console.log("Hey Im cosole")
+console.log("this is second")
+console.log("Hey Im cosole")
+
+var a = prompt("Enter your number: ")
+var isTrue = confirm("Your sure you want to leave this site? ")
+
+if(isTrue){
+    console.log("blasting computer");
+}
+else{
+    console.log("Your computer is safe");
+}
+
+console.log("Your phone number is " + a)
+
+document.title = "Hey, I am good"
+
+document.body.style.backgroundColor = "brown"
