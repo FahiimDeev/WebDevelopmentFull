9e7a9d4c5736e5865d192c4e14a3f72bf2d4f56a
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-02-14 06:20:02 +0600

    Add files via upload

---
+++ Course55 (Js data types)/script.js
@@ -0,0 +1,32 @@
+console.log("hey this is a tutorial")
+
+let a = 10, b = 20;
+let c = "fahim";
+
+console.log(a+b+8)
+console.log(typeof a, typeof b, typeof c)
+
+const bb = 56;
+// bb = 44;
+
+console.log(bb)
+
+let x = 10;
+let y = 13;
+let z = 4.66;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);
+
+let o = {
+    "name" : "fahim",
+    "job code" : 5556,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "4234cr";
+console.log(o); 
