c9df95a57aa97bb0e4ca636e0e5fabbc6c7183f2
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-02-14 08:31:50 +0600

    Add files via upload

---
+++ Course56 (JavaScript Conditionals)/index.js
@@ -0,0 +1,54 @@
+// console.log("Hello I am a conditional something")
+
+// let age = 6;
+// let grace = 6;
+
+// console.log(age+grace)
+// console.log(age-grace)
+// console.log(age*grace)
+// console.log(age/grace)
+// console.log(age**grace)
+// console.log(age%grace)
+
+
+// if((age==grace)){
+//     console.log("You can drive")
+// }
+// else{
+//     console.log("you can't drive")
+// }
+
+// let a = 35;
+// let b = 67;
+// let c = 30;
+
+// if (a > b) {
+//     if (a > c) {
+//         console.log("a=" + a + " is greater than b and c");
+//     }
+// } else if (b > a) {
+//     if (b > c) {
+//         console.log("b=" + b + " is greater than a and c");
+//     }
+// } else if (c > a) {
+//     if (c > b) {
+//         console.log("c=" + c + " is greater than a and b");
+//     }
+// }
+
+// let a = 35;
+// let b = 67;
+// let c = 30;
+
+// let max = Math.max(a,b,c);
+
+// if (max == a){
+//     console.log("a=" + a + " is greater than b and c");
+// }
+// else if (max == b){
+//     console.log("b=" + b + " is greater than a and c");
+// }
+// else {
+//     console.log("c=" + c + " is greater than a and b");
+// }
+
