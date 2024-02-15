25e842bd0942a1c7a5370118cbab3b374f685da9
Mustafa Fahim <91342968+FahiimDeev@users.noreply.github.com>
2024-02-15 16:55:48 +0600

    Add files via upload

---
+++ Course57 (JavaScript loops)/index.js
@@ -0,0 +1,44 @@
+console.log("I am a tutorial on loop")
+
+let a = 1
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i =0;i<100;i++){
+//     console.log(a+i)
+// }
+
+// let obj = {
+//     name: "Fahim",
+//     role: "Programmer",
+//     company: "Native"
+// }
+
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key, element)   
+//     }
+
+// for (const c of "Fahim") {
+//     console.log(c)
+// }
+
+// let i = 0;
+// while (i<100){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+//     i+=5;
+// }
+
+let myObject = {
+    Height: 5.7,
+    weight: 70,
+    color: "Chocolate"
+}
+
+for (const iterator of 5.7) {
+    console.log(iterator)    
+}
